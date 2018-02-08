2a414e77075d5ed6fc8b6484e2c48fb386a86617
sam <baskicisamet@gmail.com>
2018-02-08 23:52:04 +0300

    Adding URI Component Builder

--- src/main/java/com/sam/springrestclientexample/services/ApiServiceImpl.java
+++ src/main/java/com/sam/springrestclientexample/services/ApiServiceImpl.java
@@ -2,8 +2,10 @@ package com.sam.springrestclientexample.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.sam.api.domain.User;
 import com.sam.api.domain.UserData;
@@ -11,12 +13,14 @@ import com.sam.api.domain.UserData;
 @Service
 public class ApiServiceImpl implements ApiService {
 	
-	private RestTemplate RestTemplate;
+	private RestTemplate restTemplate;
 		
+	private final String api_url;
 
-	public ApiServiceImpl(org.springframework.web.client.RestTemplate restTemplate) {
-		super();
-		RestTemplate = restTemplate;
+	public ApiServiceImpl(org.springframework.web.client.RestTemplate restTemplate, @Value("${api.url}") String api_url) {
+		this.restTemplate = restTemplate;
+		this.api_url = api_url;
+		
 	}
 
 	
@@ -25,7 +29,13 @@ public class ApiServiceImpl implements ApiService {
 	@Override
 	public List<User> getUsers(Integer limit) {
 		
-		UserData userData = RestTemplate.getForObject("http://apifaketory.com/api/user?limit=" + limit, UserData.class);
+		UriComponentsBuilder uriBuilder = UriComponentsBuilder
+				.fromUriString(api_url)
+				.queryParam("limit", limit);
+		
+		
+		
+		UserData userData = restTemplate.getForObject(uriBuilder.toUriString(), UserData.class);
 		return userData.getData();
 	}
 
